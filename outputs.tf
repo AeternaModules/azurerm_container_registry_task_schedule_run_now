64a2489999a34d7ab3abc02c9c14d1f5da14e3c7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:09 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "container_registry_task_schedule_run_nows_id" {
+  description = "Map of id values across all container_registry_task_schedule_run_nows, keyed the same as var.container_registry_task_schedule_run_nows"
+  value       = { for k, v in azurerm_container_registry_task_schedule_run_now.container_registry_task_schedule_run_nows : k => v.id }
+}
 output "container_registry_task_schedule_run_nows_container_registry_task_id" {
   description = "Map of container_registry_task_id values across all container_registry_task_schedule_run_nows, keyed the same as var.container_registry_task_schedule_run_nows"
   value       = { for k, v in azurerm_container_registry_task_schedule_run_now.container_registry_task_schedule_run_nows : k => v.container_registry_task_id }
