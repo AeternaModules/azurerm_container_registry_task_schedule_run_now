fa34de9ddf48e1e1361f22e3091145118a80f441
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:31 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,5 @@
-output "container_registry_task_schedule_run_nows" {
-  description = "All container_registry_task_schedule_run_now resources"
-  value       = azurerm_container_registry_task_schedule_run_now.container_registry_task_schedule_run_nows
-}
 output "container_registry_task_schedule_run_nows_container_registry_task_id" {
-  description = "List of container_registry_task_id values across all container_registry_task_schedule_run_nows"
-  value       = [for k, v in azurerm_container_registry_task_schedule_run_now.container_registry_task_schedule_run_nows : v.container_registry_task_id]
+  description = "Map of container_registry_task_id values across all container_registry_task_schedule_run_nows, keyed the same as var.container_registry_task_schedule_run_nows"
+  value       = { for k, v in azurerm_container_registry_task_schedule_run_now.container_registry_task_schedule_run_nows : k => v.container_registry_task_id }
 }
 
